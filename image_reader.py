981e0eb0b952764e4b6f46c56d22dc27f01d2146
AjithK14 <kemisettia@gmail.com>
2019-09-23 22:04:59 -0500

    image reading

--- image_reader.py
+++ image_reader.py
@@ -1,4 +1,7 @@
-import matplotlib.image as img
-#images are 80x80
-image = img.imread("platinum/1.png")
-print(image[40])
+from PIL import Image
+
+img = Image.open('platinum/1.png')
+for i in range(4,80):
+	new_img = img.resize((i,i))
+
+# new_img.save("newimg.png", "PNG", optimize=True)
