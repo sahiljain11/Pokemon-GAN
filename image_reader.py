7ccad105aa4480b4dde20dc0c252b4fd92e1e6c0
AjithK14 <kemisettia@gmail.com>
2019-09-23 22:09:08 -0500

    image method

--- image_reader.py
+++ image_reader.py
@@ -1,7 +1,11 @@
 from PIL import Image
 
+def change_resolution(img,newsize):
+	#change img resolution to newsize x newsize
+	return img.resize((newsize,newsize))
+
 img = Image.open('platinum/1.png')
 for i in range(4,80):
-	new_img = img.resize((i,i))
+	new_img = change_resolution(img)
 
 # new_img.save("newimg.png", "PNG", optimize=True)
