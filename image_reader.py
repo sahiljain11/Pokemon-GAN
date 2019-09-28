2e91557a527c03c058fd5389db06e0c70c786c3a
AjithK14 <kemisettia@gmail.com>
2019-09-28 12:56:00 -0500

    fixed image resolution bs

--- image_reader.py
+++ image_reader.py
@@ -1,11 +1,12 @@
 from PIL import Image
+import numpy as np
 
 def change_resolution(img,newsize):
 	#change img resolution to newsize x newsize
 	return img.resize((newsize,newsize))
 
 img = Image.open('platinum/1.png')
-for i in range(4,80):
-	new_img = change_resolution(img)
+for i in range(50,51):
+	new_img = change_resolution(change_resolution(img,i),80)
 
-# new_img.save("newimg.png", "PNG", optimize=True)
+new_img.save("snewimg.png", "PNG", optimize=True)
